64b3103a61b5fb5ea6db95b21be2eb7ee37dcdf2
Filip Wiatrowski <filip.wiatrowski@infotech.edu.pl>
2025-06-12 12:17:44 +0200

    zmieniłem homepage

--- app/src/components/main_body/homePage/HomePage.js
+++ app/src/components/main_body/homePage/HomePage.js
@@ -17,27 +17,11 @@ function HomePage(props) {
 
   return (
     <div className={styles.body}>
-      {/* popularne kategorie */}
-      <div className={styles.pupularne}>
-        <NavLink to="/template" className={styles.element}>
-          <h3 className={styles.elementText}>Example</h3>
-        </NavLink>
-        <NavLink to="/template" className={styles.element}>
-          <h3 className={styles.elementText}>Example</h3>
-        </NavLink>
-        <h2 className={styles.text}>Popularne kategorie</h2>
-        <NavLink to="/template" className={styles.element}>
-          <h3 className={styles.elementText}>Example</h3>
-        </NavLink>
-        <NavLink to="/template" className={styles.element}>
-          <h3 className={styles.elementText}>Example</h3>
-        </NavLink>
-      </div>
       {/* popularne przepis */}
-      popularne
       <div className={styles.pupularne}>
         {popularRecipes.map((recipe) => (
-          <NavLink to={recipe._id} className={styles.element}>
+          // className={styles.element}
+          <NavLink to={recipe._id} style={{ backgroundImage: `url(${recipe.img})`, textDecoration: "none", color: "black", width: "15%", height: "100px", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <h3 className={styles.elementText}>{recipe.nazwa}</h3>
           </NavLink>
         ))}
